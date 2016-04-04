cfac391627603a3c880dcc72b13fdf7e6980e6dd
Joe <joe@bloggs.com>
2016-04-04 14:16:39 +0100

    First commit.

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jim on 04/04/16.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("This is a simple test project!");
+    }
+}
