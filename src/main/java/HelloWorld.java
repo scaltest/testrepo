0aab1425817ca1fb73cdcf1b8464dd9316e3a1e2
Joe <joe@bloggs.com>
2016-04-04 14:19:04 +0100

    Wrapped in different method.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,10 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        printMessages();
+    }
+
+    private static void printMessages() {
         System.out.println("Hello, world!");
         System.out.println("This is a simple test project!");
     }
